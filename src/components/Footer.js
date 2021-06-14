3b4a570fd46110f2c7c40bde2f5338bf44e2c8e6
cyrine <amricyrine@gmail.com>
2021-06-14 12:53:23 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+            copyRights for F1
+            <h1>GoodBye</h1>
+        </div>
+    )
+}
+
+
+export const Title = () => {
+    return (
+        <div>
+            <h1>Bonjour</h1>
+        </div>
+    )
+}
